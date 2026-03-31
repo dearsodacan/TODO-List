53bdb2bf80a1084e19bfdf33083c7b3fc8391606
Jacob M. <241238814+dearcanada@users.noreply.github.com>
2026-03-31 19:51:29 +0800

    Deployment commit

--- src/scripts/ui/actsModalHandlers.d.ts
+++ src/scripts/ui/actsModalHandlers.d.ts
@@ -1,2 +1,7 @@
+type lastModalCallerType = {
+    caller: HTMLButtonElement | null;
+};
 export declare const modal: HTMLDialogElement;
+export declare const lastModalCaller: lastModalCallerType;
+export {};
 //# sourceMappingURL=actsModalHandlers.d.ts.map
